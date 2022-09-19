4fef03563a47869134b530b755a3baf254215dd9
James Gabriel Dumaug <jamesgabrieldumaug@gmail.com>
2022-09-18 23:33:53 -0600

    background image for main section

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Head from 'next/head';
+import Main from '../components/Main';
 
 export default function Home() {
   return (
@@ -9,11 +10,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <div>
-        <h1 className="p-20 bg-blue-400 text-center text-5xl text-bold">
-          HELLO
-        </h1>
-      </div>
+      <Main />
     </div>
   );
 }
